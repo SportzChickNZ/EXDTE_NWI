ededd9c91d6366de00d1f6949a39c01b29922c7d
Nicky <168635521+SportzChickNZ@users.noreply.github.com>
2025-02-17 10:43:10 +1300

    Add files via upload
    added multiplication
    and trialled f string instead of format

--- 02_input_math.py
+++ 02_input_math.py
@@ -10,21 +10,6 @@
 name = input("What is your name? ")
 
 # ask user for two numbers
-
-# add numbers together
-
-# multiply numbers together
-
-# greet user and display math
-print('Hello')
-print("Hello", name)
-
-# get input
-
-# ask user for name
-name = input("What is your name? ")
-
-# ask user for two numbers
 num_1 = int(input("What is your favourite number? "))
 num_2 = int(input("What is your second favourite number?"))
 
@@ -32,8 +17,11 @@ num_2 = int(input("What is your second favourite number?"))
 add = num_1 + num_2
 
 # multiply numbers together
+mult = num_1 * num_2
 
 # greet user and display math
 print("Hello", name)
-
 print("{} + {} = {}".format(num_1, num_2, add))
+print("{} * {} = {}".format(num_1, num_2, mult))
+
+print(f'{num_1} * {num_2} = {multi}')
